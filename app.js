6d7811b6d3d2b4c9d9388d328763551dc8ef405a
Vinicius Yonezawa <vinicius.yonezawa@gmail.com>
2021-12-09 01:17:37 -0300

    page html

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
-const http = require('http')
+const path = require('path')
+const http = require('http');
+
 const app = express();
 app.set('port', process.env.PORT || 2222);
+app.use(express.static(path.join(__dirname, 'public')))
 
 const server = http.createServer(app).listen(app.get('port'), () => { 
     console.log('server listening port ' + app.get('port'));
