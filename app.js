b8f7e577f943734609b5c6d3ca76e3925bdc8333
Vinicius Yonezawa <vinicius.yonezawa@gmail.com>
2021-12-09 02:04:13 -0300

    socket events

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const path = require('path')
+const path = require('path');
 const http = require('http');
 
 const app = express();
@@ -10,4 +10,45 @@ const server = http.createServer(app).listen(app.get('port'), () => {
     console.log('server listening port ' + app.get('port'));
 });
 
+const io = require('socket.io')(server);
+io.listen(server);
+
+let users = []
+
+io.on('connection', (socket) => { 
+
+    socket.on('connect', () => { 
+        console.log('new connection - socket.id: ' + socket.id);
+    })
+
+    socket.on('disconnect', ()=> { 
+        console.log(socket.nickname + "has disconnected")
+        const updateUsers = users.filter(user => user != socket.nickname)
+        console.log("updatedUsers: ", updateUsers)
+        users = updateUsers
+        io.emit('userList', users)
+    })
+
+    socket.on('nick', (nickname) => { 
+        console.log("nick => nickname : ", nickname)
+        socket.nickname = nickname
+        users.push(nickname)
+
+        console.log("server : users :", users)
+        io.emit('userList', users);
+
+    });
+
+    socket.on('chat', (data)=> { 
+        console.log("chat => nickname : ", socket.nickname)
+        const d = new Date()
+        const ts = d.toLocaleString
+        console.log("ts : ", ts)
+        const response = `${ts} : ${socket.nickname} : ${data.message}`
+        console.log("ts : ", response)
+        io.emit('chat', response)
+    });
+
+})
+    
 
